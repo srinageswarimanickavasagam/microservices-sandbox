54780fe89e05441489032816f849736779ec13e0
Srinageswari <srinageswari.m@gmail.com>
2024-03-04 12:19:50 +0530

    Add service registry

---
+++ service-registry/src/test/java/srinageswari/lEarn/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package srinageswari.lEarn.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
