4786e2516388188be2eb734192e815b35c4db374
Srinageswari <srinageswari.m@gmail.com>
2024-03-04 14:11:21 +0530

    Add config server and refactor product & order service configurations

---
+++ config-server/src/test/java/srinageswari/lEarn/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package srinageswari.lEarn.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
